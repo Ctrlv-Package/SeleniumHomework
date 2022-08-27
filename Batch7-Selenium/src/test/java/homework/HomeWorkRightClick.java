5c654d33d3c934fa7aab15cec772ec8f8ebf3ec7
pedro <pedro@DESKTOP-1EHHS4J>
2022-08-27 10:00:01 -0400

    Right Click Homework

---
+++ Batch7-Selenium/src/test/java/homework/HomeWorkRightClick.java
@@ -0,0 +1,60 @@
+package homework;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class HomeWorkRightClick {
+
+//	Test case 1: Right Click 
+
+	WebDriver driver;
+
+	public void setUp() {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\pedro\\OneDrive\\Desktop\\B7-Selenium\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		driver.manage().window().maximize();
+
+	}
+
+	public void tearDown() throws InterruptedException {
+
+		Thread.sleep(3000);
+		driver.quit();
+
+	}
+
+	public void rightClickTest() throws InterruptedException {
+
+		driver.get("https://demo.guru99.com/test/simple_context_menu.html");
+
+		Actions action = new Actions(driver);
+
+		WebElement rightClickButton = driver.findElement(By.xpath("//*[contains(text(), 'right click me')]"));
+
+		action.contextClick(rightClickButton).build().perform();
+
+		WebElement copyButton = driver
+				.findElement(By.xpath("//*[@class='context-menu-list context-menu-root']//li[3]"));
+
+		copyButton.click();
+
+		String getText = driver.switchTo().alert().getText();
+
+		System.out.println("Get the text: " + getText);
+
+		driver.switchTo().alert().accept();
+
+	}
+
+}
