22b1d8e31feed021afc592ca7fd2aaefddefded7
pedro <pedro@DESKTOP-1EHHS4J>
2022-08-28 18:18:23 -0400

    finished

--- Batch7-Selenium/src/test/java/homework/HomeWorkAddNewJob.java
+++ Batch7-Selenium/src/test/java/homework/HomeWorkAddNewJob.java
@@ -1,12 +1,13 @@
 package homework;
 
-import java.util.concurrent.TimeUnit;
 
+
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+
 import org.testng.Assert;
 
 public class HomeWorkAddNewJob {
